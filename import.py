170d80aee96c5cd6405aeaa62da304b4f93333a8
Muktan <muktan@pop-os.localdomain>
2020-12-14 22:20:15 +0530

    ENH: updated the structure to ensure loose coupling

--- import.py
+++ import.py
@@ -1,4 +1,4 @@
-#Importing libraries
+# Importing libraries
 import os
 import zipfile
 import pandas as pd
@@ -9,15 +9,18 @@ import logging
 import pickle
 import dill
 import warnings
+
 warnings.filterwarnings("ignore")
 import sys
 import tensorflow as tf
 import numpy as np
 import re
 import librosa
-import seaborn as sns; sns.set()
+import seaborn as sns
+
+sns.set()
 import IPython.display as ipd
-from scipy.io import wavfile #for audio processing
+from scipy.io import wavfile  # for audio processing
 from string import ascii_lowercase
 import matplotlib.pyplot as plt
 from __future__ import division
@@ -28,9 +31,5 @@ import difflib
 from difflib import SequenceMatcher
 from collections import defaultdict, Counter
 import librosa.display
-dill._dill._reverse_typemap['ClassType'] = type
 
-from run import start
-
-# call main function from run file
-start()
+dill._dill._reverse_typemap["ClassType"] = type
---
+++ sys_analysis.py
@@ -0,0 +1,2 @@
+def analysis():
+    #given the reference and hypothesis return analysis
